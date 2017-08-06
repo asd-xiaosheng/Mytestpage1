1b40cd3723379baeb00ab6cea2cc2ab2213a2458
asd-xiaosheng <30740872+asd-xiaosheng@users.noreply.github.com>
2017-08-06 08:27:10 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,16 +13,16 @@ var myButton = document.querySelector('Button');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
-    var myName = prompt('Please enter your name.');
+    var myName = prompt('请输入你的名字.');
     localStorage.setItem('name',myName);
-    myHeading.textContent = 'Mozilla is cool,' + myName;
+    myHeading.textContent = '剑灵大佬,' + myName;
 }
 
 if(!localStorage.getItem('name')) {
     setUserName();
 }   else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool,' + storedName;
+    myHeading.textContent = '剑灵大佬,' + storedName;
 }
 
 myButton.onclick = function() {
