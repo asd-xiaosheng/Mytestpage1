5d5a0aad28ccd85b20bc6a9dfccfb9b0d346f232
asd-xiaosheng <30740872+asd-xiaosheng@users.noreply.github.com>
2017-08-10 15:23:36 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,40 @@
-var myImage = document.querySelector('img');
+var myImage001 = document.querySelector('#meitu001');
+var myImage002 = document.querySelector('#meitu002');
 
-myImage.onclick = function(){
-    var mySrc = myImage.getAttribute('src');
+myImage001.onclick = function(){
+    var mySrc001 = myImage001.getAttribute('src');
 
-    if(mySrc ==='images/jianlingmeitu0001.jpg')
-    {myImage.setAttribute ('src','images/jianlingmeitu0002.jpg');}   
-    else if(mySrc ==='images/jianlingmeitu0002.jpg' ) 
-         {myImage.setAttribute ('src','images/jianlingmeitu0003.jpg');}    
-         else if(mySrc ==='images/jianlingmeitu0003.jpg')
-              {myImage.setAttribute ('src','images/jianlingmeitu0004.jpg');}
-              else{myImage.setAttribute ('src','images/jianlingmeitu0001.jpg');}
+    if(mySrc001 ==='images/jianlingmeitu0001.jpg')
+    {myImage001.setAttribute ('src','images/jianlingmeitu0002.jpg');}   
+    else if(mySrc001 ==='images/jianlingmeitu0002.jpg' ) 
+         {myImage001.setAttribute ('src','images/jianlingmeitu0001.jpg');}    
+}
+
+myImage002.onclick = function(){
+    var mySrc002 = myImage002.getAttribute('src');
+
+    if(mySrc002 ==='images/jianlingmeitu0003.jpg')
+    {myImage002.setAttribute ('src','images/jianlingmeitu0004.jpg');}   
+    else if(mySrc002 ==='images/jianlingmeitu0004.jpg' ) 
+         {myImage002.setAttribute ('src','images/jianlingmeitu0005.jpg');}
+         else  {myImage002.setAttribute ('src','images/jianlingmeitu0003.jpg');}
+             
 }
                  
-var myButton = document.querySelector('Button');
-var myHeading = document.querySelector('h1');
+var myButton = document.querySelector('#anniu1');
+var myH = document.querySelector('h1');
 
 function setUserName() {
-    var myName = prompt('请输入你的名字.');
+    var myName = prompt('请输入大佬的名字.');
     localStorage.setItem('name',myName);
-    myHeading.textContent = '剑灵大佬,' + myName;
+    myH.textContent = '剑灵大佬是' + myName;
 }
 
 if(!localStorage.getItem('name')) {
     setUserName();
 }   else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = '剑灵大佬,' + storedName;
+    myH.textContent = '剑灵大佬是' + storedName;
 }
 
 myButton.onclick = function() {
